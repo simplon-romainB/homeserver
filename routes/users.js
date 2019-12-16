e0350a8bbf2c68618b3044d03270a96e2e91e0e9
simplon-romainB <yysh@hotmail.fr>
2019-12-16 19:53:16 +0100

    ok

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', async(req, res, next) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM users');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('/', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 module.exports = router;
