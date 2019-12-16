02493610fbf165daf9632f15356ec6c22df5b916
simplon-romainB <yysh@hotmail.fr>
2019-12-16 20:07:58 +0100

    ok

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/', async(req, res, next) => {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM users');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('/users', results );
+    res.send( results );
     client.release();
   } catch (err) {
     console.error(err);
