77b5ab8511020ff7fc26ce55dc18d57c73a96a88
simplon-romainB <yysh@hotmail.fr>
2019-12-18 16:03:10 +0100

    ok

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+const bcrypt = require('bcryptjs');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function(req,res) {
+  bcrypt.hash("Magicstar198.", 10, function (err,hash) {
+    pool.connect().query('INSERT INTO users (user_email, user_password) VALUES ('+req.body.email+', '+hash+')')
+  });
+});
 module.exports = router;
