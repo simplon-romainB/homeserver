68facff35780396d62732fecef5f7fc5621ec4a9
simplon-romainB <yysh@hotmail.fr>
2019-12-26 13:11:08 +0100

    ok

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 let jwt = require('jsonwebtoken');
-const config = require('./config.js');
+const config = require('middleware.js');
 
 let checkToken = (req, res, next) => {
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
