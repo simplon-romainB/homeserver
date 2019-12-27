2208f5a177ea0184db381dcc62b2d285e93d120a
simplon-romainB <yysh@hotmail.fr>
2019-12-27 11:49:28 +0100

    ok

--- routes/articles.js
+++ routes/articles.js
@@ -4,6 +4,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcryptjs');
 let jwt = require('jsonwebtoken');
 const config = require('./config.js');
+var middle = require('../middleware.js');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -18,5 +19,15 @@ router.get('/', async(req,response, next) =>{
     console.log(requete)
     response.send(requete.rows);
 });
+
+router.post('/', (req,res,next) => {
+  middle.checkToken();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Authorization", req.header.token)
+  const request = "INSERT INTO users (DEFAULT,$1,$2,$3)";
+  const args = [req.body.name, req.body.text, req.body.date];
+  const client = pool.connect()
+  const requete = client.query(request,args);
+})
 
 module.exports = router;
