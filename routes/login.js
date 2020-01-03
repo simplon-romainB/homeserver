8ab5043344340bc2f6cbb848eb43ec8efc3e2db6
simplon-romainB <yysh@hotmail.fr>
2020-01-03 14:11:44 +0100

    ok

--- routes/login.js
+++ routes/login.js
@@ -15,10 +15,12 @@ router.post('/', async(req,response, next) =>{
     response.header("Access-Control-Allow-Origin", "*");
     const hash = "SELECT user_password FROM users WHERE user_email = $1"
     const role = "SELECT user_role FROM users WHERE user_email = $1"
+    const active = "SELECT user_activated FROM users WHERE user_email = $1"
     const values = [req.body.email]
     const client2 =  await pool.connect()
     const requete =  await client2.query(hash,values)
     const requete2 = await client2.query(role,values)
+    const requete3 = await client2.query(active,values)
     //let requeteJson = JSON.stringify(requete)
     let hashFinal =requete.rows[0].user_password
     let comparison = bcrypt.compare(req.body.password, hashFinal, function(err, res){
@@ -30,7 +32,7 @@ router.post('/', async(req,response, next) =>{
                 }
               );
               // return the JWT token for the future API calls
-              let reponse = JSON.stringify([token,requete2.rows[0].user_role,requete2.rows[0].user_activated])
+              let reponse = JSON.stringify([token,requete2.rows[0].user_role,requete3.rows[0].user_activated])
               response.send(reponse)
               
             
