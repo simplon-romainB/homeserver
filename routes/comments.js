6ef0e529a14b3f81e733136c27bead9a0ba6d57c
simplon-romainB <yysh@hotmail.fr>
2020-01-04 20:58:21 +0100

    ok

--- routes/comments.js
+++ routes/comments.js
@@ -23,7 +23,7 @@ router.get('/', async(req,response, next) =>{
   const request = "SELECT * FROM comments";
   const client2 =  await pool.connect()
   const requete =  await client2.query(request, (err,result) =>{ 
-    response.send(requete.rows),
+    response.send(result.rows),
     response.end(),
     client2.end();
   })
@@ -36,14 +36,13 @@ router.post('/', async(req,res,next) => {
     const foreign =  "SELECT articles_id FROM articles WHERE articles_name = $1"
     const foreignArgs = [req.body.article]
     const client = await pool.connect()
-    const requete = await client.query(foreign,foreignArgs, function(err,result){ client.end()});
-    console.log(requete)
+    const requete = await client.query(foreign,foreignArgs, (err,result) =>  console.log(result));
     const request = "INSERT INTO comments (comments_articles,comments_body, comments_id, comments_author, comments_date) VALUES ($1,$2,DEFAULT,$3,$4)";
     const args = [requete.rows[0].articles_id, req.body.comment,req.body.author, req.body.date];
     const client2 = await pool.connect()
     const requete2 = await client2.query(request,args, (err,result) => 
       
-      res.send(args),
+      res.send(result),
       res.end(),
       client2.end());
     
