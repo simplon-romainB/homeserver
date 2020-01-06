e8224c105d86d8058fdddd599efa998e9d532b34
simplon-romainB <yysh@hotmail.fr>
2020-01-06 13:58:59 +0100

    ok

--- routes/login.js
+++ routes/login.js
@@ -15,7 +15,6 @@ const pool = new Pool({
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Connection", "close")
     next();
   });
 
