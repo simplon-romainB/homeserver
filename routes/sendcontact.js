abda1e98f8d075303874ee6106c6b3359483b7e1
simplon-romainB <yysh@hotmail.fr>
2020-01-06 12:29:00 +0100

    ok

--- routes/sendcontact.js
+++ routes/sendcontact.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { Pool } = require('pg');
 var middle = require('../middleware.js');
 var app = express();
+const mailgun = require("mailgun-js");
 var nodeMailer = require('nodemailer');
 const DOMAIN = "sandbox2fb4006253504b5fa4e78cdcdf465765.mailgun.org";
 const mg = mailgun({apiKey: "5cbc1918b0dfc706e4e67fee181bd806-6f4beb0a-87f53bfc", domain: DOMAIN});
