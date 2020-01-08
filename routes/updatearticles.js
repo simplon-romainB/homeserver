26793ec1dc4f5b2e847afbbc9360ebc8e048cf2a
simplon-romainB <yysh@hotmail.fr>
2020-01-08 17:14:08 +0100

    ok

--- routes/updatearticles.js
+++ routes/updatearticles.js
@@ -12,6 +12,7 @@ const pool = new Pool({
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
   next();
 });
 
