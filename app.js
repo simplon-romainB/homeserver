638b0d04ecc35413e5829feb3108da544ec61d06
simplon-romainB <yysh@hotmail.fr>
2020-01-09 13:40:02 +0100

    ok

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dbRouter = require('./routes/db');
@@ -31,23 +32,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
- 
+app.use(cors())
 
-  // authorized headers for preflight requests
-  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-  //res.header('Access-Control-Allow-Headers', '*');
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-
-  app.options('*', (req, res) => {
-      // allowed XHR methods  
-      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-      res.send();
-  });
   
-});
+
 
 
 app.use('/', indexRouter);
