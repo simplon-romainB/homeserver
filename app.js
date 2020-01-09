87f0b74454fea314b7965e63434898191b03fe92
simplon-romainB <yysh@hotmail.fr>
2020-01-09 13:25:44 +0100

    ok

--- app.js
+++ app.js
@@ -46,11 +46,7 @@ app.use((req, res, next) => {
       res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
       res.send();
   });
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+  
 });
 
 
