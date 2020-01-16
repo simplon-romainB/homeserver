627c8b4932a6c2607bc863d1363e37786e4d8e05
simplon-romainB <yysh@hotmail.fr>
2020-01-16 15:30:30 +0100

    ok

--- routes/postarticles.js
+++ routes/postarticles.js
@@ -20,7 +20,7 @@ const pool = new Pool({
 router.post('/', async(req,res,next) => {
   res.header('Content-Type','text/html');
   middle.checkToken(req,res,next);
-  const request = "INSERT INTO articles (articles_id,articles_name,articles_body,articles_date,articles_categorie,articles_image) VALUES (DEFAULT,$1,$2,$3,$4)";
+  const request = "INSERT INTO articles (articles_id,articles_name,articles_body,articles_date,articles_categorie,articles_image) VALUES (DEFAULT,$1,$2,$3,$4,$5)";
   const args = [req.body.titre, req.body.article, req.body.date, req.body.categorie, req.body.image];
   const client = await pool.connect()
   const requete = await client.query(request,args, (err,result) => {
